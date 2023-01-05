43c53319e3e228a7580ac188a86825e527cdf596
Oceane Marchand <oceane.mar.pro@gmail.com>
2023-01-05 13:57:29 +0100

    exo29 en cours

--- 29_myAdminClass.js
+++ 29_myAdminClass.js
@@ -1,8 +1,31 @@
-const User = require('./28_myUserClass')
+class User {
+    constructor(surname, name, age) {
+        this.surname = surname;
+        this.name = name;
+        this.age = age;
+    }
+
+    presentation() {
+        return `Je m'appelle ${this.surname} ${this.name}, j'ai ${this.age}`;
+    }
+}
+
+/*function user (surname, name, age) { 
+    let me = new User (surname, name, age);
+    return me;
+}*/
 
 class Admin extends User {
     constructor (user, role="administrateur") {
-        super(user.surname, user.name, user.age);
+        super(user.surname, user.name, user.age, user.presentation());
         this.role = role;
     }
 }
+
+let user = new User("Guilian", "Ganster", 22);
+let me = new Admin(user,);
+
+console.log(user.presentation());
+console.log(user.role);
+
+module.exports = Admin;
